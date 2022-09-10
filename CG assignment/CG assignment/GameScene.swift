525806679ec114c38f7b726f1cc9aa6e1bbdaba6
it.iosdev7 <it.iosdev7@dawateislami.net>
2022-09-10 23:34:47 +0500

    changes

--- CG assignment/CG assignment/GameScene.swift
+++ CG assignment/CG assignment/GameScene.swift
@@ -9,24 +9,45 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var x: Int = -450
+    
     
     var textureAtlas = SKTextureAtlas()
     var mainGuy = SKSpriteNode()
-    var textureArray = [SKTexture]()
+    var movingPerson = [SKTexture]()
+    var door = SKSpriteNode()
+    var doorImages = [SKTexture]()
+    var doortextureAtlas = SKTextureAtlas()
+    
+    
+    
+    
     
     override func didMove(to view: SKView) {
         textureAtlas = SKTextureAtlas(named: "img")
         NSLog("\(textureAtlas.textureNames)")
+        doortextureAtlas = SKTextureAtlas(named: "door")
+        print("\(doortextureAtlas.textureNames)")
+        
+        for i in 1...doortextureAtlas.textureNames.count-1 {
+            doorImages.append(SKTexture(imageNamed: "\(doortextureAtlas.textureNames[i]).png"))
+        }
         
         for i in 1...textureAtlas.textureNames.count-1 {
             let name = "\(textureAtlas.textureNames[i]).png"
-            textureArray.append(SKTexture(imageNamed: name))
+            movingPerson.append(SKTexture(imageNamed: name))
         }
-        mainGuy = SKSpriteNode(imageNamed: textureAtlas.textureNames[0] )
-        mainGuy.size = CGSize(width: self.frame.width/2, height: self.frame.height/2)
-        mainGuy.position = CGPoint(x: 150, y: 150)
+        
+        door = SKSpriteNode(imageNamed: doortextureAtlas.textureNames[0])
+        door.size = CGSize(width: self.frame.width/5, height: self.frame.height)
+        door.position = CGPoint(x: 420, y: 0)
+                mainGuy = SKSpriteNode(imageNamed: textureAtlas.textureNames[0] )
+        mainGuy.size = CGSize(width: self.frame.width/5, height: self.frame.height)
+        mainGuy.position = CGPoint(x: x, y: 0)
         
         self.addChild(mainGuy)
+        self.addChild(door)
+
     }
     
     override func sceneDidLoad() {
@@ -39,16 +60,15 @@ class GameScene: SKScene {
    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        mainGuy.run(SKAction.repeatForever(.animate(with: textureArray, timePerFrame: 0.5)))
-       
+        
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-     
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-       
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -57,6 +77,22 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
+        
+        if x<400 {
+            x += 4
+            moveTowardDoor()
+        }
+    }
+    
+    func moveTowardDoor()  {
       
+        let dooropen = SKAction.repeat(.animate(with: doorImages, timePerFrame: 0.2), count: 1)
+        door.run(dooropen, completion: {
+            print("Commplete")
+            let allImages = SKAction.animate(with: self.movingPerson, timePerFrame: 0.1)
+            let movePerson = SKAction.moveTo(x: CGFloat(self.x), duration: 0.5)
+            self.mainGuy.run(SKAction.repeat(SKAction.sequence([movePerson, allImages]), count: 1))
+        })
+        
     }
 }
